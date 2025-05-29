a5f5b4b3aee31a2323cec90124fe873f6983164e
ashtron <ashton.playsted@protonmail.com>
2025-05-28 23:55:16 -0700

    Use .env for environment variables

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,5 @@
+// src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-const supabaseUrl = 'https://ffehaxetotjvhgqlincr.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZWhheGV0b3RqdmhncWxpbmNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0MjM5MDAsImV4cCI6MjA2Mzk5OTkwMH0.R8t8Rt7CRZ2IYkpkFYZlm1EZ9yxmj5o-6squQTaM5YM';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
